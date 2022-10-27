0d4616aa1e9a7fe8345a66e062eb832ab9fd786c
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-26 21:22:40 -0500

    Update shark_pontoon.ts

--- codewars/8 kyu/shark_pontoon.ts
+++ codewars/8 kyu/shark_pontoon.ts
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/57e921d8b36340f1fd000059
+
 export function shark(pontoonDistance: number,
                sharkDistance: number, youSpeed: number, sharkSpeed: number, dolphin: boolean): string {
     if (dolphin) {
